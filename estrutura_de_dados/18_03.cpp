f1e90c538522612fde76a72fe99809fe68914baa
Gustavo Tironi <126670916+gtironi@users.noreply.github.com>
2024-03-18 11:35:09 +0000

    Exemplo do uso de referencia para retorno de função

--- estrutura_de_dados/18_03.cpp
+++ estrutura_de_dados/18_03.cpp
@@ -4,7 +4,8 @@ using std::cout;
 using std::string;
 using std::endl;
 
-void troca(int, int);
+void troca(int&, int&);
+int& trocaElemento(int, int[]);
 
 int main()
 {
@@ -19,15 +20,15 @@ int main()
     string& strrefNome = strNome;
     int& irefValor = iValor;
 
-    cout << "Valor da string: " << strNome << ". Endereço da string: " << &strNome << endl;
-    cout << "Valor do inteiro: " << iValor << ". Endereço do inteiro: " << &iValor << endl;
+    cout << "Valor da string: " << strrefNome << ". Endereço da string: " << &strrefNome << endl;
+    cout << "Valor do inteiro: " << irefValor << ". Endereço do inteiro: " << &irefValor << endl;
     cout << "===================================================" << endl;
 
     strrefNome = "C. Camacho";
     irefValor = 111;
 
-    cout << "Valor da string: " << strNome << ". Endereço da string: " << &strNome << endl;
-    cout << "Valor do inteiro: " << iValor << ". Endereço do inteiro: " << &iValor << endl;
+    cout << "Valor da string: " << strrefNome << ". Endereço da string: " << &strrefNome << endl;
+    cout << "Valor do inteiro: " << irefValor << ". Endereço do inteiro: " << &irefValor << endl;
     cout << "===================================================" << endl;
 
     int iNum1 = 7;
@@ -43,13 +44,33 @@ int main()
     cout << "iNum2: " << iNum2 << endl;
     cout << "===================================================" << endl;
 
+    int arriVetor[5] = {0, 7, 13, 42, 666};
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "Vetor[" << i << "] = " << arriVetor[i] << endl;
+    }
+
+    cout << "-----" << endl;
+
+    trocaElemento(3, arriVetor) = 111;
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "Vetor[" << i << "] = " << arriVetor[i] << endl;
+    }
+
     return 0;
 }
 
-void troca(int iValor1, int iValor2)
+void troca(int& irefValor1, int& irefValor2)
 {
+    int iTemp = irefValor1;
+    irefValor1 = irefValor2;
+    irefValor2 = iTemp;
+}
 
-    int iTemp = iValor1;
-    iValor1 = iValor2;
-    iValor2 = iTemp;
+int& trocaElemento(int iElemento, int arriVector[])
+{
+    return arriVector[iElemento];
 }
