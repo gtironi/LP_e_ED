50a0d9ca07bc2f471257c0143562255292205cc6
Gustavo Tironi <126670916+gtironi@users.noreply.github.com>
2024-03-18 11:16:02 +0000

    Adicionei um exemplo de variável local

--- estrutura_de_dados/18_03.cpp
+++ estrutura_de_dados/18_03.cpp
@@ -4,6 +4,8 @@ using std::cout;
 using std::string;
 using std::endl;
 
+void troca(int, int);
+
 int main()
 {
     string strNome = "Yuri Saporito";
@@ -28,5 +30,26 @@ int main()
     cout << "Valor do inteiro: " << iValor << ". Endereço do inteiro: " << &iValor << endl;
     cout << "===================================================" << endl;
 
+    int iNum1 = 7;
+    int iNum2 = 42;
+
+    cout << "iNum1: " << iNum1 << endl;
+    cout << "iNum2: " << iNum2 << endl;
+    cout << "===================================================" << endl;
+
+    troca(iNum1, iNum2);
+
+    cout << "iNum1: " << iNum1 << endl;
+    cout << "iNum2: " << iNum2 << endl;
+    cout << "===================================================" << endl;
+
     return 0;
+}
+
+void troca(int iValor1, int iValor2)
+{
+
+    int iTemp = iValor1;
+    iValor1 = iValor2;
+    iValor2 = iTemp;
 }
