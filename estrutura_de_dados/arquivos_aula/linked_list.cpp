1e42db60251a56725fa4f5bf223b49b5f46783e4
Gustavo Tironi <126670916+gtironi@users.noreply.github.com>
2024-04-10 22:16:47 +0000

    Adicionei o arquivo de linked list da aula

--- estrutura_de_dados/arquivos_aula/linked_list.cpp
+++ estrutura_de_dados/arquivos_aula/linked_list.cpp
@@ -5,14 +5,12 @@ using std::cout;
 using std::endl;
 using std::string;
 
-typedef struct Node
-{
+typedef struct Node{
     int iData;
     Node* ptrNext;
 } Node;
  
-typedef struct LinkedList
-{
+typedef struct LinkedList{
     Node* ptrFirst;
 } LinkedList;
  
@@ -26,8 +24,30 @@ void removeElement(LinkedList* const, int);
 
 int main()
 {
-	LinkedList* linkedList = newLinkedList();
+    LinkedList* linkedList = newLinkedList();
+    // LinkedList* linkedList = nullptr;
+
+    showFirstElement(linkedList);
+    showElements(linkedList);
+    showLastElement(linkedList);
+    
     cout << "====================" << endl;
+    
+    addElement(linkedList, 0);
+    addElement(linkedList, 3);
+    addElement(linkedList, 7);
+    addElement(linkedList, 13);
+    addElement(linkedList, 42);
+    
+    showFirstElement(linkedList);
+    showLastElement(linkedList);
+    showElements(linkedList);
+    cout << "====================" << endl;
+    
+    removeElement(linkedList, 666);
+    showElements(linkedList);
+    removeElement(linkedList, 7);
+    showElements(linkedList);
 
     return 0;
 }
@@ -47,4 +67,120 @@ Node* newNode(int iValue)
     temp->ptrNext = nullptr;
     
     return temp;
+}
+
+void showFirstElement(LinkedList* const linkedList)
+{
+    if(linkedList == nullptr || linkedList->ptrFirst == nullptr)
+    {
+        cout << "showFirstElement: Lista Vazia" << endl;
+    }
+    else
+    {
+        cout << "Primeiro da Lista: " << linkedList->ptrFirst->iData << endl;
+    }
+}
+
+void showLastElement(LinkedList* const linkedList)
+{
+    if(linkedList == nullptr || linkedList->ptrFirst == nullptr)
+    {
+        cout << "showLastElement: Lista Vazia" << endl;
+    }
+    else
+    {
+        Node* lastNode = linkedList->ptrFirst;
+        
+        while (lastNode->ptrNext != nullptr)
+        {
+            lastNode = lastNode->ptrNext;
+        }
+        
+        cout << "Último da Lista: " << lastNode->iData << endl;
+    }
+}
+
+void showElements(LinkedList* const linkedList)
+{
+    if(linkedList == nullptr || linkedList->ptrFirst == nullptr)
+    {
+        cout << "showElements: Lista Vazia" << endl;
+    }
+    else
+    {
+        Node* current = linkedList->ptrFirst;
+        
+        cout << "Elementos da Lista:";
+        
+        while (current != nullptr)
+        {
+            cout << " " << current->iData;
+            current = current->ptrNext;
+        }
+        cout << endl;
+    }
+}
+
+void addElement(LinkedList* const linkedList, int iValue)
+{
+    Node* temp = newNode(iValue);
+    
+    if(linkedList == nullptr)
+    {
+        cout << "addElement: Lista é nula" << endl;
+        return;
+    }
+    
+    if(linkedList->ptrFirst == nullptr)
+    {
+        linkedList->ptrFirst = temp;
+    }
+    else
+    {
+        Node* current = linkedList->ptrFirst;
+        
+        while (current->ptrNext != nullptr)
+        {
+            current = current->ptrNext;
+        }
+        
+        current->ptrNext = temp;
+    }
+}
+
+void removeElement(LinkedList* const linkedList, int iValue)
+{
+    if(linkedList == nullptr || linkedList->ptrFirst == nullptr)
+    {
+        cout << "removeElement: Lista Vazia" << endl;
+        return;
+    }
+    
+    Node* current = linkedList->ptrFirst;
+    Node* previous = nullptr;
+    
+    // Case 1: Removing the first node
+    if (current->iData == iValue)
+    {
+        linkedList->ptrFirst = current->ptrNext;
+        free(current);
+        return;
+    }
+    
+    // Case 2: Removing a node in the middle
+    while (current != nullptr && current->iData != iValue)
+    {
+        previous = current;
+        current = current->ptrNext;
+    }
+    
+    if (current == nullptr)
+    {
+        cout << "Elemento não encontrado na lista" << endl;
+        return;
+    }
+    
+    previous->ptrNext = current->ptrNext;
+    free(current);
+    return;
 }
